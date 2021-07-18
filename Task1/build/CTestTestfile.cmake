064a6ed161560b51948a4d1b11b2f30903878229
Tkacheva <kate2000_00@mail.ru>
2021-07-18 11:23:38 +0300

    corrected comments

--- Task1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/patricia/WorkSpace/protei/IT_School-21/Task1
-# Build directory: /home/patricia/WorkSpace/protei/IT_School-21/Task1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
